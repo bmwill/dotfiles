6a417986ae6794cbbfa27ffbc44b66e331c7e1df
Brandon Williams <bmw014@eng.ucsd.edu>
2016-02-26 16:31:59 -0800

    added bin and notes folders and added dotfiles/bin to path

---
+++ bin/rename.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+for file in "$@"
+do
+    newName=`echo $file | tr ' ' '_' | tr -d '[{}(),\!]' | tr -d "\'" | sed 's/_-_/_/g' | sed 's/_USA//g' | sed 's/\&/and/g'`
+    newName=`echo $newName | sed 's/_EnFr//g'`
+    mv -v "$file" "$newName"
+done
