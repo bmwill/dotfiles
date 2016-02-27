40def48250f3251966618ef3489c32b308a1c9ab
Brandon Williams <bmw014@eng.ucsd.edu>
2016-02-27 12:25:00 -0800

    Added homebrew install script and empty Brewfile

---
+++ homebrew/install-homebrew.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Install Homebrew
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+exit 0
