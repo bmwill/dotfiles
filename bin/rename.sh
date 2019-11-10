60c58c36a933002636a24a10299215d2ba89dba4
Brandon Williams <bmwill@fb.com>
2019-11-10 08:57:16 -0800

    bin: remove rename.sh

--- bin/rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for file in "$@"
-do
-  newName=`echo $file | tr ' ' '_' | tr -d '[{}(),\!]' | tr -d "\'" | sed 's/_-_/_/g' | sed 's/_USA//g' | sed 's/\&/and/g'`
-  newName=`echo $newName | sed 's/_EnFr//g'`
-  mv -v "$file" "$newName"
-done
