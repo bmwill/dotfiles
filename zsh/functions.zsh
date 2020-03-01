d50b94ebd25cf5f4877260c6501baa91f57b85ba
Brandon Williams <bmwill@fb.com>
2020-03-01 13:31:27 -0800

    zsh: add functions.zsh file

---
+++ zsh/functions.zsh
@@ -0,0 +1,61 @@
+# Use 'view' to read manpages, if u want colors, regex - search.
+function vman() {
+    man $1 | col -b | \
+        view \
+        -c 'map q :q<CR>' \
+        -c 'hi StatusLine ctermbg=green| set ft=man nomod nolist' -
+}
+
+# grep for running process, like: 'any vim'
+function any() {
+    emulate -L zsh
+    unsetopt KSH_ARRAYS
+    if [[ -z "$1" ]] ; then
+        echo "any - grep for process(es) by keyword" >&2
+        echo "Usage: any <keyword>" >&2 ; return 1
+    else
+        ps xauwww | grep -i -e "[${1[1]}]${1[2,-1]}"
+    fi
+}
+
+# Provide useful information on zsh globbing
+# See zshexpn(1) for more info
+function help-zshglob() {
+    cat <<EOF
+    /      directories
+    .      plain files
+    @      symbolic links
+    =      sockets
+    p      named pipes (FIFOs)
+    *      executable plain files (0100)
+    %      device files (character or block special)
+    %b     block special files
+    %c     character special files
+    r      owner-readable files (0400)
+    w      owner-writable files (0200)
+    x      owner-executable files (0100)
+    A      group-readable files (0040)
+    I      group-writable files (0020)
+    E      group-executable files (0010)
+    R      world-readable files (0004)
+    W      world-writable files (0002)
+    X      world-executable files (0001)
+    s      setuid files (04000)
+    S      setgid files (02000)
+    t      files with the sticky bit (01000)
+
+    print *(m-1)          # Files modified up to a day ago
+    print *(a1)           # Files accessed a day ago
+    print *(@)            # Just symlinks
+    print *(Lk+50)        # Files bigger than 50 kilobytes
+    print *(Lk-50)        # Files smaller than 50 kilobytes
+    print **/*.c          # All *.c files recursively starting in \$PWD
+    print **/*.c~file.c   # Same as above, but excluding 'file.c'
+    print (foo|bar).*     # Files starting with 'foo' or 'bar'
+    print *~*.*           # All Files that do not contain a dot
+    chmod 644 *(.^x)      # make all plain non-executable files publically readable
+    print -l *(.c|.h)     # Lists *.c and *.h
+    print **/*(g:users:)  # Recursively match all files that are owned by group 'users'
+    echo /proc/*/cwd(:h:t:s/self//) # Analogous to >ps ax | awk '{print \$1}'<
+EOF
+}
