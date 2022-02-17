fd12cc5c712e4fdc181ed72fbe7d03ddde9d7f2f
Brandon Williams <bwilliams.eng@gmail.com>
2022-02-16 22:14:06 -0800

    nvim: add sumneko_lua lsp server

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -32,7 +32,7 @@ local function setup()
 
   local config = {
     -- disable virtual text
-    virtual_text = true,
+    virtual_text = false,
     -- show signs
     signs = {
       active = signs,
@@ -208,6 +208,25 @@ lsp_installer.on_server_ready(function(server)
         rust_tools.setup(rustopts)
         server:attach_buffers()
     else
+        if server.name == "sumneko_lua" then
+            local sumneko_opts = {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            library = {
+                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                [vim.fn.stdpath("config") .. "/lua"] = true,
+                            },
+                        },
+                    },
+                },
+            }
+
+            opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+        end
         -- This setup() function is exactly the same as lspconfig's setup function.
         -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
         server:setup(opts)
